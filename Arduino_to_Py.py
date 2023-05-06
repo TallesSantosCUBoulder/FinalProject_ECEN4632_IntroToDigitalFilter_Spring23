79096db5f271753e962887d5ee9b1d7f7c2316bb
Diego Sena <132620866+Diego-Sena-Boulder@users.noreply.github.com>
2023-05-06 12:52:36 -0600

    Updated Arduino Test

--- Arduino_to_Py.py
+++ Arduino_to_Py.py
@@ -1,2 +1,31 @@
 import serial
-import numpy
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+data = []
+fig, ax = plt.subplots()
+ser = serial.Serial('COM4', 9600, timeout=1)
+line = ser.readline()
+print(line)
+
+def animate(i, data, ser):
+    if ser.in_waiting:
+        line = ser.readline()
+        string = line.decode().strip()
+        num = float(string)
+        print(num)
+        data.append(num)
+
+    data = data[-100:]
+
+    ax.clear()
+    ax.plot(data)
+    ax.set_title('Plot of Serial Data From Arduino')
+    ax.set_xlabel('rads')
+    ax.set_ylabel('Amplitude')
+
+ani = FuncAnimation(fig, animate, fargs=(data, ser), interval = 100)
+
+
+
+plt.show()
--- Test/Test.ino
+++ Test/Test.ino
@@ -4,14 +4,17 @@ void setup() {
   Serial.println("Hello World");
 }
 
-int i = 0;
+int angle = 0;
 unsigned long curr = millis();
 unsigned long last = 0;
-unsigned long delay = 1000;
+unsigned long timeDelay = 100;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  if( curr - last >= delay ){
-    Serial.println(i);
-    i++:
+  if( (curr - last) >= timeDelay ){
+    last = millis();
+    Serial.println(sin(angle * M_PI / 180));
+    angle++;
   }
+  curr = millis();
 }
