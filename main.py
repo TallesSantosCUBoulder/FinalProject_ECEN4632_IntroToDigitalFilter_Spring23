9201975e689253e851785103f4fdc72762848b40
TallesSantosCUBoulder <TallesSantos@colorado.edu>
2023-05-11 14:06:57 -0600

    Missing file.

--- main.py
+++ main.py
@@ -1,111 +1,100 @@
-# Program to indentify faces using python library and give coordinates (right or left)
+# Program to identify faces using python library and give coordinates (right or left)
 
 
 ## includes
 import face_recognition
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
-from matplotlib import image as mpimg
-import os
-import time
-
+import pickle
 
 # load the reference picture
-picture_of_me = face_recognition.load_image_file("Garrett_Image.jpg")
-my_face_encoding = face_recognition.face_encodings(picture_of_me)[0] # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
-face_landmarks_list_picture_of_me = face_recognition.face_landmarks(picture_of_me)
+picture_of_me = face_recognition.load_image_file("Diego_Image.jpeg")
+datafile = open("encodedData.dat", 'rb')
+datafile2 = open("landmarkData.dat", 'rb')
+my_face_encoding = pickle.load(datafile)  # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+face_landmarks_list_picture_of_me = pickle.load(datafile2)
+datafile.close()
+datafile2.close()
+
+# take the picture using the shell script
+#os.system("./TakePicture.sh") Method 1
+# Method 2
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+result, image = cam.read()
+if result:
+    cv2.imwrite("CurrentPicture.jpg", image)
+else:
+    print("No image detected. Pleas try again!")
+
+cam.release()
 
 
-count = 0
-while count<10:
+# load the picture
+unknown_picture = face_recognition.load_image_file("CurrentPicture.jpg")
 
-    count = count + 1 ;
+# face recognition
+unknown_face_encoding = face_recognition.face_encodings(unknown_picture)
 
-    # take the picture using the shell script
-    os.system("./TakePicture.sh")
+# face landmarks
+face_landmarks_list_unknown_picture = face_recognition.face_landmarks(unknown_picture)
+picture_of_me_location = face_recognition.face_locations(unknown_picture)
 
-    # load the picture
-    unknown_picture = face_recognition.load_image_file("CurrentPicture.jpg")
+# compare faces
+n_people = np.size(unknown_face_encoding,0);
 
-    # face recognition
-    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)
+mean_x = 0
 
-    # face landmarks
-    face_landmarks_list_unknown_picture = face_recognition.face_landmarks(unknown_picture)
-    picture_of_me_location = face_recognition.face_locations(unknown_picture)
+if unknown_face_encoding:
 
-    # compare faces
-    n_people = np.size(unknown_face_encoding,0);
+    results = face_recognition.compare_faces(my_face_encoding, unknown_face_encoding)
 
-    mean_x = 0 ;
-
-    if unknown_face_encoding:
-
-        results = face_recognition.compare_faces(my_face_encoding, unknown_face_encoding)
-
-        results = np.array(results)
-        x = np.where(results == True)
+    results = np.array(results)
+    x = np.where(results == True)
 
 
-        if np.size(x,1) == True:
-            str_aux = ' Its a picture of me! Position %d' %(x[0][0])
+    if np.size(x,1) == True:
+        str_aux = ' Its a picture of me! Position %d' %(x[0][0])
 
 
-            position_face_recognition = x[0][0]
-            face_landmarks_list_unknown_picture[position_face_recognition]
+        position_face_recognition = x[0][0]
+        face_landmarks_list_unknown_picture[position_face_recognition]
 
 
-            # plot the face landmarks
+        # plot the face landmarks
 
 
-            plt.rcParams["figure.figsize"] = [7.00, 3.50]
-            plt.rcParams["figure.autolayout"] = True
-            im = unknown_picture
+        plt.rcParams["figure.figsize"] = [7.00, 3.50]
+        plt.rcParams["figure.autolayout"] = True
+        im = unknown_picture
 
-            fig, ax = plt.subplots()
-            im = ax.imshow(im)
+        fig, ax = plt.subplots()
+        im = ax.imshow(im)
 
-            x_center_face = np.array( picture_of_me_location )
-            ax.plot(x_center_face[0,1], x_center_face[0,0], marker='v', color="pink")
-            ax.plot(x_center_face[0,1], x_center_face[0,2], marker='v', color="pink")
-            ax.plot(x_center_face[0,3], x_center_face[0,0], marker='v', color="pink")
-            ax.plot(x_center_face[0,3], x_center_face[0,2], marker='v', color="pink")
+        x_center_face = np.array( picture_of_me_location )
+        ax.plot(x_center_face[0,1], x_center_face[0,0], marker='v', color="pink")
+        ax.plot(x_center_face[0,1], x_center_face[0,2], marker='v', color="pink")
+        ax.plot(x_center_face[0,3], x_center_face[0,0], marker='v', color="pink")
+        ax.plot(x_center_face[0,3], x_center_face[0,2], marker='v', color="pink")
 
-            mean_x = (x_center_face[0,1]+x_center_face[0,3])/2
-            mean_y = (x_center_face[0,0]+x_center_face[0,2])/2
+        mean_x = (x_center_face[0,1]+x_center_face[0,3])/2
+        mean_y = (x_center_face[0,0]+x_center_face[0,2])/2
 
-            ax.plot(mean_x, mean_y, marker='v', color="black")
-
-        else:
-            str_aux = "It's not a picture of me!"
-
+        ax.plot(mean_x, mean_y, marker='v', color="black")
 
     else:
-        str_aux = "No faces were recognized!"
+        str_aux = "It's not a picture of me!"
 
 
-    output_int = 0
-    if ( mean_x < np.size(unknown_picture,1)/2 ):
-        output_int = -1
-        str_go_to = "robot: go to left"
-    else:
-        output_int = 1
-        str_go_to = "robot: go to right"
-
-    if( mean_x==0 ):
-        output_int = 0
-        str_go_to = "robot: no actions"
-
-    print("\n\n\n")
-    print(str_aux)
-    print("Position: %d [1 - %d]" %(mean_x,np.size(unknown_picture,1)))
-    print(str_go_to)
-    print("\n\n\n")
-
-    plt.show()
-    plt.pause(0.01)
-
-    time.sleep(2) # in secs
+else:
+    str_aux = "No faces were recognized!"
 
 
+print("/n/n/n")
+print(str_aux)
+print(mean_x)
+print("/n/n/n")
 
+
+plt.show()
